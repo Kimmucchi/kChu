a5ccd1eee83f48f160dfb1c1dd1f30e093abb9b2
Kimmucchi <quennebelle@gmail.com>
2021-07-12 21:17:31 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import discord
 from io import BytesIO
 from discord.ext import commands, tasks
 
-TOKEN = ('')
+TOKEN = ('ODYwOTM1NTQ1MjE5MTg2NzA4.YOCetQ.o_eUohyYQ8bYGlA6PWjbGlTmgzM')
 intents = discord.Intents().all()
 bot = commands.Bot(command_prefix='..', intents=intents)
 bot.load_extension('EventLeaderboard.EventLeaderboard')
